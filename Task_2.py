0b94d7a7c3b5540cc6a8f7c388bda46c9aee4a5c
OMedved <84655964+OMedved@users.noreply.github.com>
2021-05-24 14:33:54 +0300

    Delete Task_2.py

--- Task_2.py
+++
@@ -1,5 +0,0 @@
-user_secs = int(input("Print here some seconds:"))
-secs = user_secs % 60
-mins = (user_secs // 60) % 60
-hours = user_secs // 3600
-print(f"It is: {hours:02d}:{mins:02d}:{secs:02d}")
